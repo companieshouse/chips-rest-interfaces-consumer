4bcb750aef637cb09a3f89b4cc2f53e4f75db2a0
markpit <mpitten@companieshouse.gov.uk>
2020-12-14 16:36:53 +0000

    BI-6174 Switching from Codehaus xml mapper to fasterxml to fix compilation error

--- src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/processor/IncomingMessageConsumer.java
+++ src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/processor/IncomingMessageConsumer.java
@@ -1,6 +1,6 @@
 package uk.gov.companieshouse.chipsrestinterfacesconsumer.processor;
 
-import org.codehaus.jackson.map.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
