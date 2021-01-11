a797ad3b446f7e271e72eed5097e04ef3884da7c
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-01-11 15:18:14 +0000

    BI-6176 unit test for service

--- src/test/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/service/impl/MessageProcessorServiceImplTest.java
+++ src/test/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/service/impl/MessageProcessorServiceImplTest.java
@@ -5,12 +5,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestClientException;
 import uk.gov.companieshouse.chipsrestinterfacesconsumer.client.ChipsRestClient;
 import uk.gov.companieshouse.chipsrestinterfacesconsumer.common.ApplicationLogger;
 import uk.gov.companieshouse.chipsrestinterfacesconsumer.model.ChipsKafkaMessage;
+import uk.gov.companieshouse.chipsrestinterfacesconsumer.producer.MessageProducer;
 import uk.gov.companieshouse.service.ServiceException;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -19,6 +23,9 @@ class MessageProcessorServiceImplTest {
 
     @Mock
     private ChipsRestClient chipsRestClient;
+
+    @Mock
+    private MessageProducer retryMessageProducer;
 
     @Mock
     private ApplicationLogger logger;
@@ -33,5 +40,13 @@ class MessageProcessorServiceImplTest {
         messageProcessorService.processMessage(chipsKafkaMessage);
 
         verify(chipsRestClient, times(1)).sendToChips(eq(chipsKafkaMessage));
+    }
+
+    @Test
+    void checkRetryIsCalled() throws ServiceException {
+        ChipsKafkaMessage chipsKafkaMessage = new ChipsKafkaMessage();
+        doThrow(RestClientException.class).when(chipsRestClient).sendToChips(chipsKafkaMessage);
+        messageProcessorService.processMessage(chipsKafkaMessage);
+        verify(retryMessageProducer, times(1)).writeToQueue(eq(chipsKafkaMessage));
     }
 }
