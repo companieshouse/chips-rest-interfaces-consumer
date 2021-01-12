5ce451cb7befbc5ca103ed363037d1b68829b8e1
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-01-12 09:26:25 +0000

    BI-6176 configuration

--- src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/configuration/KafkaConfiguration.java
+++ src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/configuration/KafkaConfiguration.java
@@ -1,8 +1,13 @@
 package uk.gov.companieshouse.chipsrestinterfacesconsumer.configuration;
 
+import org.apache.avro.Schema;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import uk.gov.companieshouse.chipsrestinterfacesconsumer.producer.KafkaRestClient;
 import uk.gov.companieshouse.kafka.consumer.CHKafkaConsumerGroup;
 import uk.gov.companieshouse.kafka.consumer.ConsumerConfig;
 import uk.gov.companieshouse.kafka.deserialization.DeserializerFactory;
@@ -15,6 +20,12 @@ import java.util.Collections;
 
 @Configuration
 class KafkaConfiguration {
+
+    @Value("${SCHEMA_REGISTRY_URL}")
+    private String schemaRegistryUrl;
+
+    @Value("${CHIPS_SCHEMA_URI}")
+    private String chipsSchemaUri;
 
     @Value("${kafka.group.name}")
     private String groupName;
@@ -30,6 +41,14 @@ class KafkaConfiguration {
 
     @Value("${kafka.producer.retries}")
     private int retries;
+
+    @Bean
+    @Qualifier("chips-kafka-send")
+    public Schema fetchChipsKafkaSendSchema(KafkaRestClient restClient) throws JSONException {
+        byte[] bytes = restClient.getSchema(schemaRegistryUrl, chipsSchemaUri);
+        String schemaJson = new JSONObject(new String(bytes)).getString("schema");
+        return new Schema.Parser().parse(schemaJson);
+    }
 
     @Bean
     DeserializerFactory getDeserializerFactory() {
