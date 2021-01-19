0284d1feed97e0fd757d41ab8586261cb169a955
markpit <mpitten@companieshouse.gov.uk>
2021-01-19 12:17:00 +0000

    BI-6579 Simplified config for MessageConsumers

--- src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/configuration/ApplicationConfig.java
+++ src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/configuration/ApplicationConfig.java
@@ -1,18 +1,14 @@
 package uk.gov.companieshouse.chipsrestinterfacesconsumer.configuration;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import uk.gov.companieshouse.chipsrestinterfacesconsumer.common.ApplicationLogger;
 import uk.gov.companieshouse.chipsrestinterfacesconsumer.consumer.MessageConsumer;
 import uk.gov.companieshouse.chipsrestinterfacesconsumer.consumer.MessageConsumerImpl;
 import uk.gov.companieshouse.chipsrestinterfacesconsumer.retry.KafkaRestClient;
-import uk.gov.companieshouse.chipsrestinterfacesconsumer.service.MessageProcessorService;
 import uk.gov.companieshouse.kafka.consumer.CHKafkaConsumerGroup;
-import uk.gov.companieshouse.kafka.deserialization.DeserializerFactory;
 
 @Configuration
 public class ApplicationConfig {
@@ -28,32 +24,12 @@ public class ApplicationConfig {
     }
 
     @Bean("incoming-message-consumer")
-    public MessageConsumer incomingMessageConsumer(AutowireCapableBeanFactory beanFactory,
-                                                   ApplicationLogger logger,
-                                                   @Qualifier("incoming-consumer-group") CHKafkaConsumerGroup consumer,
-                                                   MessageProcessorService messageProcessorService,
-                                                   DeserializerFactory deserializerFactory) {
-        MessageConsumer messageConsumer = new MessageConsumerImpl(
-                logger,
-                consumer,
-                messageProcessorService,
-                deserializerFactory);
-        beanFactory.initializeBean(messageConsumer, "incoming-message-consumer");
-        return messageConsumer;
+    public MessageConsumer incomingMessageConsumer(@Qualifier("incoming-consumer-group") CHKafkaConsumerGroup consumer) {
+        return new MessageConsumerImpl(consumer, "incoming-message-consumer");
     }
 
     @Bean("retry-message-consumer")
-    public MessageConsumer retryMessageConsumer(AutowireCapableBeanFactory beanFactory,
-                                                ApplicationLogger logger,
-                                                @Qualifier("retry-consumer-group") CHKafkaConsumerGroup consumer,
-                                                MessageProcessorService messageProcessorService,
-                                                DeserializerFactory deserializerFactory) {
-        MessageConsumer messageConsumer = new MessageConsumerImpl(
-                logger,
-                consumer,
-                messageProcessorService,
-                deserializerFactory);
-        beanFactory.initializeBean(messageConsumer, "retry-message-consumer");
-        return messageConsumer;
+    public MessageConsumer retryMessageConsumer(@Qualifier("retry-consumer-group") CHKafkaConsumerGroup consumer) {
+        return new MessageConsumerImpl(consumer, "incoming-message-consumer");
     }
 }
--- src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/consumer/MessageConsumerImpl.java
+++ src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/consumer/MessageConsumerImpl.java
@@ -1,5 +1,6 @@
 package uk.gov.companieshouse.chipsrestinterfacesconsumer.consumer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.companieshouse.chips.ChipsRestInterfacesSend;
 import uk.gov.companieshouse.chipsrestinterfacesconsumer.common.ApplicationLogger;
 import uk.gov.companieshouse.chipsrestinterfacesconsumer.service.MessageProcessorService;
@@ -15,13 +16,18 @@ import java.util.Map;
 
 public class MessageConsumerImpl implements MessageConsumer {
 
+    @Autowired
     private ApplicationLogger logger;
+
+    @Autowired
+    private MessageProcessorService messageProcessorService;
+
+    @Autowired
+    private DeserializerFactory deserializerFactory;
 
     private CHKafkaConsumerGroup consumer;
 
-    private MessageProcessorService messageProcessorService;
-
-    private DeserializerFactory deserializerFactory;
+    private String id;
 
     @PostConstruct
     void init() {
@@ -33,33 +39,29 @@ public class MessageConsumerImpl implements MessageConsumer {
         consumer.close();
     }
 
-    public MessageConsumerImpl(ApplicationLogger logger,
-                               CHKafkaConsumerGroup consumer,
-                               MessageProcessorService messageProcessorService,
-                               DeserializerFactory deserializerFactory) {
-        this.logger = logger;
+    public MessageConsumerImpl(CHKafkaConsumerGroup consumer,
+                               String id) {
         this.consumer = consumer;
-        this.messageProcessorService = messageProcessorService;
-        this.deserializerFactory = deserializerFactory;
+        this.id = id;
     }
 
     @Override
     public void readAndProcess() {
         for (Message msg : consumer.consume()) {
             try {
-                logger.info(String.format("Message offset %s retrieved, processing", msg.getOffset()));
+                logger.info(String.format("%s - Message offset %s retrieved, processing", id, msg.getOffset()));
                 ChipsRestInterfacesSend deserializedMsg = deserialize(msg);
                 Map<String, Object> logMap = new HashMap<>();
                 logMap.put("Message Offset", msg.getOffset());
                 logMap.put("Deserialised message id", deserializedMsg.getMessageId());
-                logger.info("Message deserialised successfully", logMap);
+                logger.info(String.format("%s - Message deserialised successfully", id), logMap);
                 messageProcessorService.processMessage(deserializedMsg);
-                logger.info(String.format("Message offset %s processed, committing offset", msg.getOffset()));
+                logger.info(String.format("%s - Message offset %s processed, committing offset", id, msg.getOffset()));
                 consumer.commit(msg);
             } catch (Exception e) {
                 Map<String, Object> data = new HashMap<>();
                 data.put("message", msg.getValue() == null ? "" : new String(msg.getValue()));
-                logger.error("Failed to read message from queue", e, data);
+                logger.error(String.format("%s - Failed to read message from queue", id), e, data);
             }
         }
     }
