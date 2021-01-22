a26bf89224d5bbd7a75833eac76fc975495f4763
ICoxCH <ICox@companieshouse.gov.uk>
2021-01-22 09:10:29 +0000

    Allow the app to be started in error processing mode

--- src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/Application.java
+++ src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/Application.java
@@ -2,10 +2,12 @@ package uk.gov.companieshouse.chipsrestinterfacesconsumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import uk.gov.companieshouse.chipsrestinterfacesconsumer.common.ApplicationLogger;
 import uk.gov.companieshouse.chipsrestinterfacesconsumer.service.LoopingMessageProcessor;
 
 import java.util.concurrent.CompletableFuture;
@@ -24,16 +26,29 @@ public class Application implements CommandLineRunner {
     @Qualifier("retry-looping-consumer")
     private LoopingMessageProcessor loopingRetryMessageConsumer;
 
+    @Autowired
+    private ApplicationLogger logger;
+
+    @Value("${IS_ERROR_CONSUMER}")
+    private boolean isErrorConsumer;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
 
     @Override
     public void run(String... args) {
-        var mainCompletableFuture = loopingMainMessageConsumer.loopReadAndProcess();
-        var retryCompletableFuture = loopingRetryMessageConsumer.loopReadAndProcess();
+        if (!isErrorConsumer) {
+            logger.info(String.format("%s started in normal processing mode", APPLICATION_NAME));
+            var mainCompletableFuture = loopingMainMessageConsumer.loopReadAndProcess();
+            var retryCompletableFuture = loopingRetryMessageConsumer.loopReadAndProcess();
 
-        // Wait until they are all done
-        CompletableFuture.allOf(mainCompletableFuture, retryCompletableFuture).join();
+            // Wait until they are all done
+            CompletableFuture.allOf(mainCompletableFuture, retryCompletableFuture).join();
+        } else {
+            logger.info(String.format("%s started in error processing mode", APPLICATION_NAME));
+
+            //ToDo Start error consumer
+        }
     }
 }
---
+++ src/test/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/ApplicationTest.java
@@ -0,0 +1,53 @@
+package uk.gov.companieshouse.chipsrestinterfacesconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import uk.gov.companieshouse.chipsrestinterfacesconsumer.common.ApplicationLogger;
+import uk.gov.companieshouse.chipsrestinterfacesconsumer.service.LoopingMessageProcessor;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ApplicationTest {
+
+    @Mock
+    private ApplicationLogger logger;
+
+    @Mock
+    private LoopingMessageProcessor loopingMainMessageConsumer;
+
+    @Mock
+    private LoopingMessageProcessor loopingRetryMessageConsumer;
+
+    @InjectMocks
+    private Application application;
+
+    @Test
+    void runErrorProcessingTrueTest() {
+        ReflectionTestUtils.setField(application, "isErrorConsumer", true);
+        application.run();
+
+        verify(loopingMainMessageConsumer, times(0)).loopReadAndProcess();
+        verify(loopingRetryMessageConsumer, times(0)).loopReadAndProcess();
+    }
+
+    @Test
+    void runErrorProcessingFalseTest() {
+        when(loopingMainMessageConsumer.loopReadAndProcess()).thenReturn(CompletableFuture.completedFuture(true));
+        when(loopingRetryMessageConsumer.loopReadAndProcess()).thenReturn(CompletableFuture.completedFuture(true));
+
+        ReflectionTestUtils.setField(application, "isErrorConsumer", false);
+        application.run();
+
+        verify(loopingMainMessageConsumer, times(1)).loopReadAndProcess();
+        verify(loopingRetryMessageConsumer, times(1)).loopReadAndProcess();
+    }
+}
