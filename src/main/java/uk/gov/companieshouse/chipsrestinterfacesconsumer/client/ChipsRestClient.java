2bb6f20e9f5fbe92773a8e8ba3ba687c644e8d3a
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-01-25 14:28:04 +0000

    BI-6582 import not used

--- src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/client/ChipsRestClient.java
+++ src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/client/ChipsRestClient.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriTemplate;
 import uk.gov.companieshouse.chips.ChipsRestInterfacesSend;
