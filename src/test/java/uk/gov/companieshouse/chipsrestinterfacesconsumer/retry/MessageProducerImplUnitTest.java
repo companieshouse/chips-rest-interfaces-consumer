a88d35686203a873a807d60698a19452b3bfc292
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-01-26 12:08:11 +0000

    BI-6582 imports

--- src/test/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/retry/MessageProducerImplUnitTest.java
+++ src/test/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/retry/MessageProducerImplUnitTest.java
@@ -22,7 +22,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
