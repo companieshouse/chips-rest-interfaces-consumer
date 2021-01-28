c1b8be1d8ae28af983fa348cb62d9758c7efc802
markpit <mpitten@companieshouse.gov.uk>
2021-01-28 11:21:53 +0000

    BI-6742 Removing unused import for HttpStatus

--- src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/service/impl/MessageProcessorServiceImpl.java
+++ src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/service/impl/MessageProcessorServiceImpl.java
@@ -2,7 +2,6 @@ package uk.gov.companieshouse.chipsrestinterfacesconsumer.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
 import uk.gov.companieshouse.chips.ChipsRestInterfacesSend;
