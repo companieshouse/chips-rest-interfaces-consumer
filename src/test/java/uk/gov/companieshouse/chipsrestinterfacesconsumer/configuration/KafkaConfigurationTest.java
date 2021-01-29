64bbf35662608a503672cc4a685c00d9b5d909b2
ICoxCH <ICox@companieshouse.gov.uk>
2021-01-29 13:44:03 +0000

    test bean declaration

--- src/test/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/configuration/KafkaConfigurationTest.java
+++ src/test/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/configuration/KafkaConfigurationTest.java
@@ -20,9 +20,11 @@ class KafkaConfigurationTest {
 
     private static final String MAIN_GROUP_NAME_VALUE = "main-test-group";
     private static final String RETRY_GROUP_NAME_VALUE = "retry-test-group";
+    private static final String ERROR_GROUP_NAME_VALUE = "error-test-group";
     private static final String BROKER_ADDRESS_VALUE = "kafka address";
     private static final String MAIN_TOPIC_NAME_VALUE = "main-topic";
     private static final String RETRY_TOPIC_NAME_VALUE = "retry-topic";
+    private static final String ERROR_TOPIC_NAME_VALUE = "error-topic";
     private static final int POLL_TIMEOUT_VALUE = 100;
     private static final int RETRIES = 10;
 
@@ -33,9 +35,11 @@ class KafkaConfigurationTest {
         kafkaConfiguration = new KafkaConfiguration();
         ReflectionTestUtils.setField(kafkaConfiguration, "mainConsumerGroupName", MAIN_GROUP_NAME_VALUE);
         ReflectionTestUtils.setField(kafkaConfiguration, "retryConsumerGroupName", RETRY_GROUP_NAME_VALUE);
+        ReflectionTestUtils.setField(kafkaConfiguration, "errorConsumerGroupName", ERROR_GROUP_NAME_VALUE);
         ReflectionTestUtils.setField(kafkaConfiguration, "brokerAddress", BROKER_ADDRESS_VALUE);
         ReflectionTestUtils.setField(kafkaConfiguration, "mainTopicName", MAIN_TOPIC_NAME_VALUE);
         ReflectionTestUtils.setField(kafkaConfiguration, "retryTopicName", RETRY_TOPIC_NAME_VALUE);
+        ReflectionTestUtils.setField(kafkaConfiguration, "errorTopicName", ERROR_TOPIC_NAME_VALUE);
         ReflectionTestUtils.setField(kafkaConfiguration, "pollTimeout", POLL_TIMEOUT_VALUE);
         ReflectionTestUtils.setField(kafkaConfiguration, "retries", RETRIES);
     }
@@ -63,6 +67,19 @@ class KafkaConfigurationTest {
         assertEquals(BROKER_ADDRESS_VALUE, consumerConfig.getBrokerAddresses()[0]);
         assertEquals(1, consumerConfig.getTopics().size());
         assertEquals(RETRY_TOPIC_NAME_VALUE, consumerConfig.getTopics().get(0));
+        assertEquals(100, consumerConfig.getPollTimeout());
+    }
+
+    @Test
+    void getErrorConsumerConfigTest() {
+        ConsumerConfig consumerConfig = kafkaConfiguration.getErrorConsumerConfig();
+
+        assertEquals(ERROR_GROUP_NAME_VALUE, consumerConfig.getGroupName());
+        assertNotNull(consumerConfig.getBrokerAddresses());
+        assertEquals(1, consumerConfig.getBrokerAddresses().length);
+        assertEquals(BROKER_ADDRESS_VALUE, consumerConfig.getBrokerAddresses()[0]);
+        assertEquals(1, consumerConfig.getTopics().size());
+        assertEquals(ERROR_TOPIC_NAME_VALUE, consumerConfig.getTopics().get(0));
         assertEquals(100, consumerConfig.getPollTimeout());
     }
 
