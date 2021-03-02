9a7d7ddcfc2dfacb4e61fe0ebf8977ece2608153
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-03-02 17:24:02 +0000

    parent unused import

--- src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/service/MessageProcessorService.java
+++ src/main/java/uk/gov/companieshouse/chipsrestinterfacesconsumer/service/MessageProcessorService.java
@@ -2,8 +2,6 @@ package uk.gov.companieshouse.chipsrestinterfacesconsumer.service;
 
 import uk.gov.companieshouse.chips.ChipsRestInterfacesSend;
 
-import java.util.List;
-
 public interface MessageProcessorService {
     boolean processMessage(String consumerId, ChipsRestInterfacesSend message);
 }
